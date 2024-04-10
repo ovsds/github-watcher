b39e9ed33dd1e1e5772e1581be13cebf93384098
ovsds <ovsds@yandex-team.ru>
2024-04-10 20:41:39 +0200

    fix: pydantic factory class annotation

--- backend/lib/task/base/task.py
+++ backend/lib/task/base/task.py
@@ -1,23 +1,11 @@
-import typing
-
-import pydantic
-
 import lib.task.base.action as action_base
-import lib.task.base.trigger as task_trigger_base
+import lib.task.base.trigger as trigger_base
 import lib.utils.pydantic as pydantic_utils
 
 
 class TaskConfig(pydantic_utils.BaseModel, pydantic_utils.IDMixinModel):
-    triggers: typing.Annotated[
-        list[pydantic.SerializeAsAny[task_trigger_base.BaseTriggerConfig]],
-        pydantic.BeforeValidator(pydantic_utils.make_list_factory(task_trigger_base.BaseTriggerConfig.factory)),
-        pydantic.AfterValidator(pydantic_utils.check_unique_ids),
-    ]
-    actions: typing.Annotated[
-        list[pydantic.SerializeAsAny[action_base.BaseActionConfig]],
-        pydantic.BeforeValidator(pydantic_utils.make_list_factory(action_base.BaseActionConfig.factory)),
-        pydantic.AfterValidator(pydantic_utils.check_unique_ids),
-    ]
+    triggers: trigger_base.TriggerConfigListPydanticAnnotation
+    actions: action_base.ActionConfigListPydanticAnnotation
 
 
 __all__ = [
